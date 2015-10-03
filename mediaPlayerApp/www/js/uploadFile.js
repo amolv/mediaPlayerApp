b63804e51458bb4ceb23644f0e4542398ee27f82
mandar0119 <mandar.joshi@fountaintechies.com>
2015-10-03 15:10:14 +0530

    minor

---
+++ mediaPlayerApp/www/js/uploadFile.js
@@ -0,0 +1,18 @@
+angular.module('starter.controllers')
+.controller('uploadFile', ['$rootScope', '$scope', function($rootScope, $scope) {
+	$scope.imagefiles = [];
+	$scope.updateattachment = function(){
+		angular.forEach(document.getElementById("file_browse").files, function(file) {
+			var newfile = file;
+			console.log(newfile);
+			var oFReader = new FileReader();
+			oFReader.onload = function (oFREvent) {
+			$scope.imagefiles.push(oFReader.result);
+			console.log($scope.imagefiles);
+			var img = new Image();
+			console.log(img);
+			};
+			oFReader.readAsDataURL( newfile );
+		});
+	};
+}])
