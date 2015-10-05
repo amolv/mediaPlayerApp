206c5a1b75959cbe5cd7d29ba142e2a13c473af3
amolv <amolvhankalas@gmail.com>
2015-10-05 11:41:54 +0530

    pdf upload test

--- mediaPlayerApp/www/js/mergeController.js
+++ mediaPlayerApp/www/js/mergeController.js
@@ -1,4 +1,31 @@
 angular.module('starter.controllers')
 .controller('MergePdf', ['$rootScope', '$scope', function($rootScope, $scope) {
 	
+
+	$scope.showpdfincanvas = function(  ){
+		
+		PDFJS.getPdf('http://n2.transparent.sg:3000/assets/documents/1442934184799document.pdf', function getPdfHelloWorld(data) {
+		  //
+		  // Instantiate PDFDoc with PDF data
+		  //
+		  var pdf = new PDFJS.PDFDoc(data);
+		  var page = pdf.getPage(1);
+		  var scale = 1.5;
+		 
+		  //
+		  // Prepare canvas using PDF page dimensions
+		  //
+		  var canvas = document.getElementById('the-canvas');
+		  var context = canvas.getContext('2d');
+		  canvas.height = page.height * scale;
+		  canvas.width = page.width * scale;
+		 
+		  //
+		  // Render PDF page into canvas context
+		  //
+		  page.startRendering(context);
+		});
+
+	}
+
 }])
