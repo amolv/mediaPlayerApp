411a38a41e865c59f4d270332b7f15b23f4416b0
mandar0119 <mandar.joshi@fountaintechies.com>
2015-10-05 11:49:34 +0530

    display pdf code

--- mediaPlayerApp/www/js/mergeController.js
+++ mediaPlayerApp/www/js/mergeController.js
@@ -1,30 +1,14 @@
 angular.module('starter.controllers')
-.controller('MergePdf', ['$rootScope', '$scope', function($rootScope, $scope) {
+.controller('MergePdf', ['$rootScope', '$scope','pdfDelegate', function($rootScope, $scope,pdfDelegate) {
 	
 
 	$scope.showpdfincanvas = function(  ){
 		
-		PDFJS.getPdf('http://n2.transparent.sg:3000/assets/documents/1442934184799document.pdf', function getPdfHelloWorld(data) {
-		  //
-		  // Instantiate PDFDoc with PDF data
-		  //
-		  var pdf = new PDFJS.PDFDoc(data);
-		  var page = pdf.getPage(1);
-		  var scale = 1.5;
-		 
-		  //
-		  // Prepare canvas using PDF page dimensions
-		  //
-		  var canvas = document.getElementById('the-canvas');
-		  var context = canvas.getContext('2d');
-		  canvas.height = page.height * scale;
-		  canvas.width = page.width * scale;
-		 
-		  //
-		  // Render PDF page into canvas context
-		  //
-		  page.startRendering(context);
-		});
+		$scope.pdfUrl ="http://n2.transparent.sg:3000/assets/documents/1442934184799document.pdf";
+		pdfDelegate.
+		$getByHandle('my-pdf-container')
+		.load($scope.pdfUrl);
+
 
 	}
 
